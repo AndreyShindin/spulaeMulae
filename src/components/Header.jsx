9733f62535f9a090bf695f9618c0de799736d373
AndreySh <ashindin7@mail.ru>
2022-05-24 11:24:53 +0300

    from lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header className="header">
+            <a className="nav__link" href="">
+                <img className='header__image' src='https://i.pinimg.com/originals/f6/7a/3a/f67a3a261f0c952640f9c1d214d73e96.png'></img>
+            </a>
+       </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Profile(){
+    return(
+        <main className='main'>
+        <img className='main__image' src='https://c.wallhere.com/photos/22/27/2560x1600_px_landscape_nature-1077192.jpg!d'></img>
+        Main content
+        <div className='main__avatar'>
+          av + desc
+        </div>
+        <div>
+          my posts
+        </div>
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+       </main>
+    )
+}
+
+export default Profile
